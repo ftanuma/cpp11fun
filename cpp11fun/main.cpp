089749f788f437445ed5788400e8968a6242a928
Fumihiko Tanuma <ftanuma@gmail.com>
2013-11-19 22:14:46 +0900

    Add initial main.cpp

---
+++ cpp11fun/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp11fun
+//
+//  Created by Fumihiko Tanuma on 11/19/13.
+//  Copyright (c) 2013 ftanuma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
